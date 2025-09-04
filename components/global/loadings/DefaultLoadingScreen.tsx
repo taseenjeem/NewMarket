944e28589307f6a88db924b8cf74cf85a88c9c60
taseenjeem <taseenjeem.dev@gmail.com>
2025-09-04 18:43:52 +0600

    Some major updates

--- components/global/loadings/DefaultLoadingScreen.tsx
+++ components/global/loadings/DefaultLoadingScreen.tsx
@@ -2,7 +2,7 @@ import { Spinner } from "@/components/ui/spinner-dependencies";
 
 export default function DefaultLoadingScreen() {
   return (
-    <section className="flex size-full min-h-screen items-center justify-center">
+    <section className="bg-background flex size-full min-h-screen items-center justify-center">
       <div className="bg-primary-foreground flex items-center gap-3 rounded-md border-2 p-3 text-xl">
         <Spinner size={"large"} /> Loading...
       </div>
