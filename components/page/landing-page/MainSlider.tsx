9569ea24dfbb5ba390d8c706d96e5aefedbe8d53
taseenjeem <taseenjeem.dev@gmail.com>
2025-09-11 03:00:58 +0600

    removed the react slick and updated the MainSlider

--- components/page/landing-page/MainSlider.tsx
+++ components/page/landing-page/MainSlider.tsx
@@ -1,38 +1,28 @@
 "use client";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+import 'swiper/css';
 import sliderData from "@/json/main-slider.json";
 import Image from "next/image";
 import Link from "next/link";
 
-const settings = {
-  dots: false,
-  infinite: true,
-  autoplay: true,
-  autoplaySpeed: 4000,
-  speed: 500,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: false,
-};
-
 export default function MainSlider() {
   return (
     <div className="h-full w-full overflow-hidden rounded">
-      <style jsx global>{`
-        .slick-slider,
-        .slick-list,
-        .slick-track {
-          height: 100% !important;
-        }
-        .slick-slide > div {
-          height: 100%;
-        }
-      `}</style>
-      <Slider {...settings} className="h-full w-full">
+      <Swiper
+        modules={[Autoplay]}
+        slidesPerView={1}
+        spaceBetween={0}
+        loop={true}
+        autoplay={{
+          delay: 4000,
+          disableOnInteraction: false,
+        }}
+        speed={500}
+        className="h-full w-full"
+      >
         {sliderData.map(({ id, image, alt }) => (
-          <div key={id} className="h-full">
+          <SwiperSlide key={id} className="h-full">
             <Link href="/" className="block h-full">
               <div className="relative h-[343px] lg:size-full">
                 <Image
@@ -44,9 +34,9 @@ export default function MainSlider() {
                 />
               </div>
             </Link>
-          </div>
+          </SwiperSlide>
         ))}
-      </Slider>
+      </Swiper>
     </div>
   );
 }
